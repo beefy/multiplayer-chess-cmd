2c3511f9a05c8ac104a8083fd77aff1d7380a548
beefy <nate.schultz@outlook.com>
2016-07-14 18:10:07 +0000

    flip board for black

--- server.js
+++ server.js
@@ -13,6 +13,11 @@ var white_rematch = -1;
 var black_rematch = -1;
 var game_over = false;
 
+function get_board(id) {
+  if (id == white) return chess.ascii()
+  return '  '+chess.ascii().split('').reverse().join('').substr(1);
+}
+
 io.on('connection', function(socket){
   if (white == -1) {
     white = socket.id;
@@ -23,14 +28,17 @@ io.on('connection', function(socket){
     turn = white;
     not_turn = black;
     console.log("black player connected: "+black);
-    io.to(turn).emit('other client','Both players connected!\n\n'+chess.ascii()+'\n\nYour Move: ') 
-    io.to(not_turn).emit('other client','Both players connected!\n\n'+chess.ascii()+'\n\nWaiting for opponent...') 
+    io.to(turn).emit('other client','Both players connected!\n\n'+get_board(turn)+'\n\nYour Move: ') 
+    io.to(not_turn).emit('other client','Both players connected!\n\n'+get_board(not_turn)+'\n\nWaiting for opponent...') 
   }
 
   console.log('a user connected');
   socket.on('disconnect',function(){
     console.log('user disconnected');
-    io.emit('other client','opponent has left the game\n\n'+chess.ascii()); 
+    var leaveid = socket.id;
+    if (leaveid == white) { leaveid = black; }
+    else { leaveid = white; } 
+    io.emit('other client','opponent has left the game\n\n'+get_board(leaveid)); 
   });
 });
 
@@ -39,14 +47,14 @@ io.on('connection', function(socket){
     console.log('message: ' + msg);
     if (game_over) {
       if (msg != 'y' && msg != 'n') {
-        socket.emit('other client','What?\n\n'+chess.ascii()+'\n\nRematch? (y/n)')
+        socket.emit('other client','What?\n\n'+get_board(socket.id)+'\n\nRematch? (y/n)')
       } else {
         if (msg == 'n') {
 	  io.to(socket.id).emit('kick','0')
 	  io.emit('kick','1')
 	  	
         }
-        socket.emit('other client','Asking Opponent for Rematch...\n\n'+chess.ascii()+'\n\nWaiting for Opponent...') 
+        socket.emit('other client','Asking Opponent for Rematch...\n\n'+get_board(socket.id)+'\n\nWaiting for Opponent...') 
         if (socket.id == white) white_rematch = true;
 	else if (socket.id == black) black_rematch = true;
 	if (white_rematch != -1 && black_rematch != -1) {
@@ -57,27 +65,28 @@ io.on('connection', function(socket){
           black = tmp;
 	  turn = white;
           not_turn = black;
-	  io.to(turn).emit('other client','New Game!\n\n'+chess.ascii()+'\n\nYour Move: ')
-          io.to(not_turn).emit('other client','New Game!\n\n'+chess.ascii()+'\n\nWaiting for opponent...') 
+	  io.to(turn).emit('other client','New Game!\n\n'+get_board(turn)+'\n\nYour Move: ')
+          io.to(not_turn).emit('other client','New Game!\n\n'+get_board(turn)+'\n\nWaiting for opponent...') 
         } 
       } 
     } else if (turn != socket.id) {
-      socket.emit('other client','it\'s not your turn!\n\n'+chess.ascii())
+      socket.emit('other client','it\'s not your turn!\n\n'+get_board(socket.id)+'\n\nWaiting for opponent...');
     } else if (chess.moves().indexOf(msg.toString()) > -1) { 
       chess.move(msg.toString());
       if (chess.game_over()) {
         game_over = true;
-        io.emit('other client','Game Over!\n\n'+chess.ascii()+'\n\nRematch? (y/n)')
+        io.to(white).emit('other client','Game Over!\n\n'+get_board(white)+'\n\nRematch? (y/n)')
+        io.to(black).emit('other client','Game Over!\n\n'+get_board(black)+'\n\nRematch? (y/n)')
       } else {
         last_move = msg.toString(); 
-        io.to(turn).emit('other client','You played: '+last_move+'\n\n'+chess.ascii()+'\n\nWaiting for opponent...')
-        io.to(not_turn).emit('other client','Opponent played: '+last_move+'\n\n'+chess.ascii()+'\n\nYour Move: ') 
+        io.to(turn).emit('other client','You played: '+last_move+'\n\n'+get_board(turn)+'\n\nWaiting for opponent...')
+        io.to(not_turn).emit('other client','Opponent played: '+last_move+'\n\n'+get_board(not_turn)+'\n\nYour Move: ') 
         var tmp = turn
         turn = not_turn
         not_turn = tmp
       }
     } else {
-      socket.emit('other client','invalid move!\npossible moves: ' + chess.moves()+'\n\n'+chess.ascii())
+      socket.emit('other client','invalid move!\npossible moves: ' + chess.moves()+'\n\n'+get_board(socket.id))
     }
   });
 });
